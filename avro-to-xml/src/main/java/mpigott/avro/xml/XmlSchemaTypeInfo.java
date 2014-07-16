cdbabbf44c74c65fdae5e61c8a829b5359dff3a4
Michael Pigott <mpigott@gmail.com>
2014-07-16 14:30:05 -0400

    XML Schema Representation of a base node with facets.

--- avro-to-xml/src/main/java/mpigott/avro/xml/XmlSchemaTypeInfo.java
+++ avro-to-xml/src/main/java/mpigott/avro/xml/XmlSchemaTypeInfo.java
@@ -24,6 +24,7 @@ import javax.xml.namespace.QName;
 import org.apache.avro.Schema;
 import org.apache.ws.commons.schema.XmlSchemaFacet;
 import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.JsonNodeFactory;
 import org.codehaus.jackson.node.ObjectNode;
 
@@ -66,6 +67,47 @@ final class XmlSchemaTypeInfo {
 
   Map<XmlSchemaRestriction.Type, List<XmlSchemaRestriction>> getFacets() {
     return facets;
+  }
+
+  /**
+   * Creates a JSON representation of the base type and
+   * the restrictions imposed on it.  This will look like:
+   *
+   * <pre>
+   * {
+   *   "baseType": { <type info of base> },
+   *   "facets": [
+   *      {
+   *        "type": "<type: LENGTH | LENGTH_MIN | LENGTH_MAX | ...>",
+   *        "value" "<value>",
+   *        "fixed": true/false
+   *      },
+   *      ...
+   *   ]
+   * }
+   * </pre>
+   */
+  JsonNode getXmlSchemaAsJson() {
+    ObjectNode type = JsonNodeFactory.instance.objectNode();
+    type.put("baseType", xmlSchemaType);
+
+    if ((facets != null) && !facets.isEmpty()) {
+      ArrayNode facetsArray = JsonNodeFactory.instance.arrayNode();
+
+      for (Map.Entry<XmlSchemaRestriction.Type, List<XmlSchemaRestriction>> facetsForType : facets.entrySet()) {
+        for (XmlSchemaRestriction facet : facetsForType.getValue()) {
+          ObjectNode facetNode = JsonNodeFactory.instance.objectNode();
+          facetNode.put("type", facet.getType().name());
+          facetNode.put("value", facet.getValue().toString());
+          facetNode.put("fixed", facet.isFixed());
+          facetsArray.add(facetNode);
+        }
+      }
+    
+      type.put("facets", facetsArray);
+    }
+
+    return type;
   }
 
   private Schema avroSchemaType;
