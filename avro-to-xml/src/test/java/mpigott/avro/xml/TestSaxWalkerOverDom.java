f0cc277f2a914e5fb05c690be467079a1e907bd2
Michael Pigott <mpigott@gmail.com>
2014-07-20 17:22:08 -0400

    Unit test for SaxWalkerOverDom.
    I found some kind of bug in the Java SAX and DOM parsers.  If the last
    element only contains an attribute, both the DOM and SAX parsers give
    that element the URI, local name, and QName of the root element
    ("schema").  The SAX parser will give it the correct attributes, but the
    DOM parser will give it the root element's attributes as well, causing
    the TestSaxWalkerOverDom test to fail.
    
    To work around this, I added a dummy element at the end.

---
+++ avro-to-xml/src/test/java/mpigott/avro/xml/TestSaxWalkerOverDom.java
@@ -0,0 +1,351 @@
+/**
+ * Copyright 2014 Mike Pigott
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mpigott.avro.xml;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Reads the <code>src/test/resources/test_schema.xsd</code>
+ * file as an XML document and confirms the correct events
+ * are triggered.
+ *
+ * @author  Mike Pigott
+ */
+public class TestSaxWalkerOverDom {
+
+  private static class StackEntry {
+    enum Type {
+      ELEMENT,
+      TEXT
+    }
+
+    StackEntry(String v) {
+      type = Type.TEXT;
+      value = v;
+      namespace = null;
+      localName = null;
+      qName = null;
+      attributes = null;
+    }
+
+    StackEntry(String ns, String ln, String qn, Attributes attrs) {
+      type = Type.ELEMENT;
+      namespace = ns;
+      localName = ln;
+      qName = qn;
+      attributes = attrs;
+      value = null;
+    }
+
+    boolean equals(String v) {
+      return stringsEqual(value, v);
+    }
+
+    boolean equals(String ns, String ln, String qn, Attributes attrs) {
+      if (!stringsEqual(namespace, ns)
+          || !stringsEqual(localName, ln)
+          || !stringsEqual(qName, qn)
+          || (attributes.getLength() > attrs.getLength()))
+      {
+        throw new IllegalStateException("Expected element [\"" + namespace + "\", \"" + localName + "\", \"" + qName + "\", " + attributes.getLength() + " attrs] does not match actual of [\"" + ns + "\", \"" + ln + "\", \"" + qn + "\", " + attrs.getLength() + " attrs].");
+      }
+
+      for (int index = 0; index < attributes.getLength(); ++index) {
+        final String actual =
+            attrs.getValue(attributes.getURI(index), attributes.getLocalName(index));
+
+        if ( !attributes.getValue(index).equals(value) ) {
+          System.err.println("Attribute [\"" + attributes.getURI(index) + "\", \"" + attributes.getLocalName(index) + "\"] has a value of \"" + attributes.getValue(index) + "\" which does not match the actual value of \"" + actual + "\".  ");
+          return false;
+        }
+
+        if ( !attrs.getValue( attributes.getQName(index) ).equals(actual) ) {
+          System.err.println("Attribute [\"" + attributes.getQName(index) + "\" has a value of \"" + value + "\" but that does not match the actual value of \"" + attrs.getValue( attributes.getQName(index) ) + "\".");
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder str = new StringBuilder(type.name());
+      str.append(": ");
+
+      if (type.equals(Type.ELEMENT)) {
+        str.append("namespace=\"").append(namespace);
+        str.append("\", localName=\"").append(localName);
+        str.append("\", qName=\"").append(qName);
+        str.append("\", attributes={ ");
+
+        if (attributes != null) {
+          for (int index = 0; index < attributes.getLength(); ++index) {
+            str.append("[Attr: namespace=\"").append( attributes.getURI(index) );
+            str.append("\", localName=\"").append( attributes.getLocalName(index) );
+            str.append("\", qName=\"").append( attributes.getQName(index) );
+            str.append("\", value=\"").append( attributes.getValue(index) ).append("\"] ");
+          }
+        }
+
+        str.append('}');
+
+      } else if (type.equals(Type.TEXT)) {
+        str.append('\"').append(value).append('\"');
+      }
+
+      return str.toString();
+    }
+
+    private static boolean stringsEqual(String lhs, String rhs) {
+      if (((lhs != null) && (rhs != null) && lhs.equals(rhs))
+          || ((lhs == null) && (rhs == null))) {
+
+        return true;
+
+      } else {
+        throw new IllegalArgumentException("\"" + lhs + "\" does not match \"" + rhs + "\"");
+      }
+    }
+
+    Type type;
+    String namespace;
+    String localName;
+    String qName;
+    String value;
+    Attributes attributes;
+  }
+
+  private static class ContentValidator implements ContentHandler {
+
+    ContentValidator(List<StackEntry> stack) {
+      this.stack = stack;
+    }
+
+    @Override
+    public void startDocument() throws SAXException { }
+
+    @Override
+    public void endDocument() throws SAXException {
+      if ( !stack.isEmpty() ) {
+        throw new SAXException("Reaced the end of the document early; expected " + stack.size() + " more elements.");
+      }
+    }
+
+    @Override
+    public void startElement(
+        String uri,
+        String localName,
+        String qName,
+        Attributes atts) throws SAXException {
+
+      if ( stack.isEmpty() ) {
+        throw new SAXException("Element " + toString(uri, localName, qName, atts) + " is not expected; stack is empty!");
+      }
+
+      StackEntry entry = stack.get(0);
+      stack.remove(0);
+
+      if (entry.type != StackEntry.Type.ELEMENT) {
+        throw new SAXException("Expected text of (" + entry + ") but received element of (" + toString(uri, localName, qName, atts) + ").");
+      }
+
+      if ( !entry.equals(uri, localName, qName, atts) ) {
+        throw new SAXException("Expected element (" + entry + ") does not match actual (" + toString(uri, localName, qName, atts) + ").");
+      }
+    }
+
+    @Override
+    public void endElement(
+        String uri,
+        String localName,
+        String qName)
+        throws SAXException
+    {
+    }
+
+    @Override
+    public void characters(
+        char[] ch,
+        int start,
+        int length)
+        throws SAXException {
+
+      final String value = toString(ch, start, length);
+
+      if ( stack.isEmpty() ) {
+        throw new SAXException("Unexpected string \"" + value + "\"; stack is empty!");
+      }
+
+      StackEntry entry = stack.get(0);
+      stack.remove(0);
+
+      if (!entry.type.equals(StackEntry.Type.TEXT)) {
+        throw new SAXException("Unexpected string \"" + value + "\"; was expecting element (" + entry + ").");
+      }
+
+      if ( !entry.equals(value) ) {
+        throw new SAXException("Expected string \"" + entry + "\" but received \"" + value + "\".");
+      }
+    }
+
+    @Override
+    public void setDocumentLocator(Locator locator) {
+      throw new UnsupportedOperationException("This should not be called.");
+    }
+
+    @Override
+    public void startPrefixMapping(String prefix, String uri)
+        throws SAXException {
+      throw new UnsupportedOperationException("This should not be called.");
+    }
+
+    @Override
+    public void endPrefixMapping(String prefix) throws SAXException {
+      throw new UnsupportedOperationException("This should not be called.");
+    }
+
+    @Override
+    public void ignorableWhitespace(char[] ch, int start, int length)
+        throws SAXException {
+      throw new UnsupportedOperationException("This should not be called.");
+    }
+
+    @Override
+    public void processingInstruction(String target, String data)
+        throws SAXException {
+      throw new UnsupportedOperationException("This should not be called.");
+    }
+
+    @Override
+    public void skippedEntity(String name) throws SAXException {
+      throw new UnsupportedOperationException("This should not be called.");
+    }
+
+    private static String toString(String uri, String localName, String qName, Attributes attrs) {
+      StringBuilder str = new StringBuilder("namespace=\"");
+      str.append(uri).append("\", localName=\"").append(localName);
+      str.append("\", qName=\"").append(qName).append("\", attributes={ ");
+
+      for (int index = 0; index < attrs.getLength(); ++index) {
+        str.append("[Attr: namespace=\"").append( attrs.getURI(index) );
+        str.append("\", localName=\"").append( attrs.getLocalName(index) );
+        str.append("\", qName=\"").append( attrs.getQName(index) );
+        str.append("\", value=\"").append( attrs.getValue(index) ).append("\"] ");
+      }
+
+      str.append('}');
+
+      return str.toString();
+    }
+
+    private static String toString(char[] ch, int start, int length) {
+      return new String(ch, start, length);
+    }
+
+    private List<StackEntry> stack;
+  }
+
+  /**
+   * This is traversed by a SAX parser to retrieve the expected SAX events.
+   *
+   * @author  Mike Pigott
+   */
+  private static class StackBuilder extends DefaultHandler {
+
+    public StackBuilder() {
+      stack = new ArrayList<StackEntry>();
+    }
+
+    public ArrayList<StackEntry> getStack() {
+      return stack;
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+      stack.clear();
+    }
+
+    @Override
+    public void startElement(
+        String uri,
+        String localName,
+        String qName,
+        Attributes atts) throws SAXException {
+
+      stack.add( new StackEntry(uri, localName, qName, atts) );
+    }
+
+    @Override
+    public void characters(
+        char[] ch,
+        int start,
+        int length)
+        throws SAXException {
+
+      stack.add( new StackEntry( new String(ch, start, length) ) );
+    }
+
+    private final ArrayList<StackEntry> stack;
+  }
+
+  @Test
+  public void test() throws Exception {
+    final File xsdFile = new File("src\\test\\resources\\test_schema.xsd");
+    StackBuilder stackBuilder = new StackBuilder();
+
+    // Parse the document using a real SAX parser
+    SAXParserFactory spf = SAXParserFactory.newInstance();
+    spf.setNamespaceAware(true);
+    SAXParser saxParser = spf.newSAXParser();
+    saxParser.parse(xsdFile, stackBuilder);
+
+    // Parse the document using a DOM parser
+    final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    dbFactory.setNamespaceAware(true);
+    final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+    final Document doc = dBuilder.parse(xsdFile);
+
+    /* Walk the DOM, firing off SAX events, and
+     * confirm they match the real SAX events.
+     */
+    SaxWalkerOverDom walker =
+        new SaxWalkerOverDom(new ContentValidator( stackBuilder.getStack() ));
+
+    walker.walk(doc);
+  }
+
+}
